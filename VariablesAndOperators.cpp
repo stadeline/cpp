ddc2272b6e7a3e38318f2c3b25e3aa41505a848d
0reo27 <stopspammingme@hotmail.ph>
2018-07-15 14:55:44 +0800

    Turbo C++ Syntax.

--- VariablesAndOperators.cpp
+++ VariablesAndOperators.cpp
@@ -1,22 +1,23 @@
-#include<iostream.h>
-#include<conio.h>
-main() {
-	
-clrscr();
+#include <iostream.h>
+#include <conio.h>
+main()
+{
+
+	clrscr();
+
+	// Declare a variable. Available are char, char string [anyInteger], int, float, double.
+	char someVariable[1];
 
 	// Display a string to the console
-	cout<<"";
-	
-	// Declare a variable. Available are char, char string [anyInteger], int, float, double.
-	char someVariable [20];
-	
+	cout << "\nEnter a string: ";
+
 	// Pipe a variable from what a user inputs.
-	cin>>someVariable;
-	
+	// This can't take spaces :(
+	cin >> someVariable;
+
 	// Display a variable to the console.
-	cout<<someVariable;
-	
+	cout << "\nThe string typed was: " << someVariable;
+
 	// Pause the console.
 	getch();
-
 }
